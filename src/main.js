fbf971ee939c0b46763a86081e787d78bb8ef1c2
Igor Rogoza <krab336@mail.ru>
2024-02-18 20:09:23 +0300

    Изменение основных файлов проекта, установка `scss`

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-import './assets/main.css'
+import './assets/scss/main.scss'
 
-import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createApp } from 'vue'
 
 import App from './App.vue'
 import router from './router'

87c42c61a480f5461ed62d577a788ddbba811371
Igor Rogoza <krab336@mail.ru>
2024-02-25 20:48:25 +0300

    Исправления в About

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,10 +1,10 @@
 <script setup></script>
 
 <template>
-  <div class="flex justify-between">
-    <div class="flex flex-col gap-y-[39px]">
-      <h1 class="text-black text-center">О нашем проекте</h1>
-      <p>
+  <div class="flex justify-between flex-col gap-y-[31px]">
+    <h1 class="text-black text-center">О нашем проекте</h1>
+    <div class="flex gap-x-[72px]">
+      <p class="text-[32px] leading-10">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam lectus
         risus, finibus ornare vestibulum et, feugiat quis dui. Vivamus sit amet
         dolor et magna facilisis rhoncus. Curabitur maximus est sed porta
@@ -20,10 +20,10 @@
         odio. Phasellus feugiat turpis quis mollis lacinia. Sed ac orci et nisi
         venenatis pharetra ac non arcu.
       </p>
-    </div>
-    <div class="flex flex-col gap-y-[51px] items-center">
-      <img src="../assets/img/gost.svg" width="360" height="95" alt="ГОСТ" />
-      <div class="w-[394px] h-[394px] bg-[#D9D9D9]"></div>
+      <div class="flex flex-col gap-y-[51px] items-center">
+        <img src="../assets/img/gost.svg" width="360" height="95" alt="ГОСТ" />
+        <div class="w-[394px] h-[394px] bg-[#D9D9D9]"></div>
+      </div>
     </div>
   </div>
 </template>
